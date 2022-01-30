fb83413ced39b84d56dedd21c0c05675de554b7a
GabsonRoger <gabsonwona@gmail.com>
2022-01-30 16:35:53 -0300

    initial commit

---
+++ src/com/dio/gof/Tests.java
@@ -0,0 +1,47 @@
+package com.dio.gof;
+
+import com.dio.gof.singleton.SingletonEager;
+import com.dio.gof.singleton.SingletonLazy;
+import com.dio.gof.singleton.SingletonLazyHolder;
+import com.dio.gof.strategy.*;
+
+public class Tests {
+    public static void main(String[] args) {
+
+        // Singleton
+        SingletonLazy lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+        lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+
+        SingletonEager eager = SingletonEager.getInstance();
+        System.out.println(eager);
+        eager = SingletonEager.getInstance();
+        System.out.println(eager);
+
+        SingletonLazyHolder lazyHolder = SingletonLazyHolder.getInstance();
+        System.out.println(lazyHolder);
+        lazyHolder = SingletonLazyHolder.getInstance();
+        System.out.println(lazyHolder);
+
+
+        // Strategy
+
+        Comportamento normal = new ComportamentoNormal();
+        Comportamento defensivo = new ComportamentoDefensivo();
+        Comportamento agressivo = new ComportamentoAgressivo();
+
+        Robo robo = new Robo();
+        robo.setComportamento(normal);
+        robo.mover();
+        robo.mover();
+        robo.setComportamento(defensivo);
+        robo.mover();
+        robo.mover();
+        robo.setComportamento(agressivo);
+        robo.mover();
+        robo.mover();
+        robo.mover();
+
+    }
+}
